f1e84f48b1fcbddc54241d835e931df77c5f25b7
Dramane-H <hidramane@yahoo.fr>
2023-06-22 11:31:30 +0200

    Update me

--- aws-train/modules/vaults/vault_aws_secret_backend.tf
+++ aws-train/modules/vaults/vault_aws_secret_backend.tf
@@ -1,4 +1,4 @@
- resource "vault_aws_secret_backend" "aws" {
+/*  resource "vault_aws_secret_backend" "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   path       = "${var.name}-path"
@@ -6,3 +6,4 @@
   default_lease_ttl_seconds = "120"
   max_lease_ttl_seconds     = "240"
 }
+ */
