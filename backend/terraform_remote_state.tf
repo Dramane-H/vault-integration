df21c8696e0baa5d9a23f666ca6b4ac6a84bd452
Dramane-H <77085556+Dramane-H@users.noreply.github.com>
2023-06-25 18:23:51 +0200

    Delete backend directory

--- backend/terraform_remote_state.tf
+++
@@ -1,7 +0,0 @@
-data "terraform_remote_state" "admin" {
-  backend = "local"
-
-  config = {
-    path = "../terraform.tfstate"
-  }
-}  
--- backend/vault_aws_access_credential.tf
+++
@@ -1,4 +0,0 @@
-data "vault_aws_access_credentials" "creds" {
-  backend = data.terraform_remote_state.admin.outputs.backend
-  role    = data.terraform_remote_state.admin.outputs.role
-} 
