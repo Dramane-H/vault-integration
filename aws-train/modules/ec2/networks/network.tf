bc3f1e40fb2e84285b9e1582cf3ba1007391dd20
Dramane-H <hidramane@yahoo.fr>
2023-07-02 00:26:52 +0200

    adding network block

---
+++ aws-train/modules/ec2/networks/network.tf
@@ -0,0 +1,75 @@
+  # Adding route table
+
+resource "aws_route_table" "example" {  
+  vpc_id = aws_default_vpc.default.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "example"
+  }
+}
+
+#  adding internet gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_default_vpc.default.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+#  adding vpc. will work on the default one
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+# adding secrurity group
+
+resource "aws_security_group" "sg_employedirect" {
+  name        = "allow_traffic"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description      = "allow https traffic"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "HTTPS"
+    cidr_blocks      = [aws_default_vpc.default.cidr_block]
+    ipv6_cidr_blocks = [aws_default_vpc.default.cidr_block]
+  }
+
+  ingress {
+    description      = "allow http traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "HTTP"
+    cidr_blocks      = [aws_default_vpc.default.cidr_block]
+    ipv6_cidr_blocks = [aws_default_vpc.default.cidr_block]
+  }
+
+  ingress {
+    description      = "allow ssh connection"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "SSH"
+    cidr_blocks      = [aws_default_vpc.default.cidr_block]
+    ipv6_cidr_blocks = [aws_default_vpc.default.cidr_block]
+  }
+
+    ingress {
+    description      = "allow TCP connection"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "TCP"
+    cidr_blocks      = [aws_default_vpc.default.cidr_block]
+    ipv6_cidr_blocks = [aws_default_vpc.default.cidr_block]
+    }
+
+}
